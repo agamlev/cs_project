31a76331fb1c2455379371be92adc1079d7cbb62
agamlev <94647604+agamlev@users.noreply.github.com>
2025-05-12 20:05:39 +0300

    Update jira_fetch.py

--- jira_fetch.py
+++ jira_fetch.py
@@ -3,12 +3,12 @@ from requests.auth import HTTPBasicAuth
 import os
 import json
 
-# הגדרות Jira
 JIRA_DOMAIN = "https://arbox.atlassian.net"
 EMAIL = "agam@arboxapp.com"
 API_TOKEN = os.environ.get("JIRA_API_TOKEN")
+
 HP_FIELDS = ["customfield_10243", "customfield_10244"]
-MAPPING_FILE = "hp_mapping.json"  # שם הקובץ שבו נשמור את המיפוי
+MAPPING_FILE = "custom_hp_mapping.json"  # שמו של קובץ ה-JSON
 
 def fetch_hp_to_issue():
     url = f"{JIRA_DOMAIN}/rest/api/3/search"
@@ -43,10 +43,9 @@ def fetch_hp_to_issue():
 def save_hp_mapping(mapping):
     with open(MAPPING_FILE, "w", encoding="utf-8") as f:
         json.dump(mapping, f, ensure_ascii=False, indent=2)
-    print(f"מיפוי נשמר בקובץ {os.path.abspath(MAPPING_FILE)}")  # הדפסת הנתיב המוחלט של הקובץ
+    print(f"מיפוי נשמר בקובץ {MAPPING_FILE}")
 
-# הרצה לבדיקה מקומית
 if __name__ == "__main__":
-    mapping = fetch_hp_to_issue()  # משיכת המיפוי מ-Jira
-    print(mapping)  # הדפס את המיפוי
-    save_hp_mapping(mapping)  # שמור את המיפוי בקובץ JSON
+    mapping = fetch_hp_to_issue()
+    print(mapping)
+    save_hp_mapping(mapping)
